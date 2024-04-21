deb033a0a42c8de7860cb3644c174d774fa2d04d
Tahir <i.tahirnazir@gmail.com>
2024-04-21 17:29:03 +0500

    ✨ module 2 : plugins

--- middleware/console.global.ts
+++ middleware/console.global.ts
@@ -1,4 +1,4 @@
 export default defineNuxtRouteMiddleware((to,from)=>{
-console.log('MiddleWare!',{to, from});    
+console.log('MiddleWare!');    
 
 })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,5 +31,9 @@ export default defineNuxtConfig({
       'composables/**',
 
     ]
-  }
+  },
+
+  plugins:[
+    '~/plugins/foo/my-plugin'
+  ]
 })
